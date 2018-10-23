80769e26afb3a6cf973cbbd291976e993c56f8c6
ice-melt <ice-melt@outlook.com>
2018-10-23 16:51:13 +0800

    Code for generating tfrecord files

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# Hand Writing Distinguish
-> tinymind手写汉字识别
+# Calligraphy Distinguish
+
+> 相关**说明**和**数据集**见[[TinyMind 汉字书法识别]](https://www.tinymind.cn/competitions/41#dataDescription)
+
+## tfrecord
+相关代码
+
+- [download_and_convert_data.py](download_and_convert_data.py)
+- [datasets/convert_quiz.py](datasets/convert_quiz.py)
+- [datasets/dataset_utils.py](datasets/dataset_utils.py)
+
+以上代码修改部分来源[https://gitee.com/ai100/quiz-w7-code](https://gitee.com/ai100/quiz-w7-code)
+
+根目录下运行命令
+```Bash
+# DATASET_DIR 是数据所在目录，需要下载和指定
+DATASET_DIR=~tmp/CalligraphyDistinguish/DATASET/train
+python download_and_convert_data.py \
+  --dataset_name=quiz \
+  --dataset_dir=${DATASET_DIR}
+```
--- download_and_convert_data.py
+++ download_and_convert_data.py
@@ -39,6 +39,7 @@ import tensorflow as tf
 from datasets import download_and_convert_cifar10
 from datasets import download_and_convert_flowers
 from datasets import download_and_convert_mnist
+from datasets import convert_quiz
 
 FLAGS = tf.app.flags.FLAGS
 
@@ -54,20 +55,23 @@ tf.app.flags.DEFINE_string(
 
 
 def main(_):
-  if not FLAGS.dataset_name:
-    raise ValueError('You must supply the dataset name with --dataset_name')
-  if not FLAGS.dataset_dir:
-    raise ValueError('You must supply the dataset directory with --dataset_dir')
+    if not FLAGS.dataset_name:
+        raise ValueError('You must supply the dataset name with --dataset_name')
+    if not FLAGS.dataset_dir:
+        raise ValueError('You must supply the dataset directory with --dataset_dir')
 
-  if FLAGS.dataset_name == 'cifar10':
-    download_and_convert_cifar10.run(FLAGS.dataset_dir)
-  elif FLAGS.dataset_name == 'flowers':
-    download_and_convert_flowers.run(FLAGS.dataset_dir)
-  elif FLAGS.dataset_name == 'mnist':
-    download_and_convert_mnist.run(FLAGS.dataset_dir)
-  else:
-    raise ValueError(
-        'dataset_name [%s] was not recognized.' % FLAGS.dataset_name)
+    if FLAGS.dataset_name == 'cifar10':
+        download_and_convert_cifar10.run(FLAGS.dataset_dir)
+    elif FLAGS.dataset_name == 'flowers':
+        download_and_convert_flowers.run(FLAGS.dataset_dir)
+    elif FLAGS.dataset_name == 'mnist':
+        download_and_convert_mnist.run(FLAGS.dataset_dir)
+    elif FLAGS.dataset_name == 'quiz':
+        convert_quiz.run(FLAGS.dataset_dir)
+    else:
+        raise ValueError(
+            'dataset_name [%s] was not recognized.' % FLAGS.dataset_name)
+
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
